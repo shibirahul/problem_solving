bf786049fcce718710216ea567931ed8c637a94c
Shibi Rahul S <shibirahul007@gmail.com>
2026-04-19 23:31:53 -0700

    Added two sum solution

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+# Problem: Two Sum
+# Link: https://leetcode.com/problems/two-sum/
+
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
